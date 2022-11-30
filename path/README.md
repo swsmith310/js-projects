ef74f69d42a522c9d1d2085fee46eb4605864043
Spencer W. Smith <spencerwayne310@gmail.com>
2022-11-30 15:05:37 -0400

    added path.js

---
+++ path/README.md
@@ -0,0 +1,3 @@
+# path.js
+
+A simple example of using lambda function as a callback to control an AI's path.
---
+++ path/game.js
@@ -0,0 +1,71 @@
+class Guard {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.xv = 0;
+        this.yv = 0;
+    }
+
+    move(path) {
+        path();
+    }
+
+    draw() {
+        fill(255, 0, 0);
+        circle(this.x, this.y, 32);
+    }
+}
+
+let A = new Guard(320, 240);
+let B = new Guard(320, 240);
+
+function setup() {
+    createCanvas(640, 480);
+}
+
+function draw() {
+    background(0);
+    A.move(() => {
+        if (A.x == 320 && A.y == 240) {
+            A.xv = 1;
+            A.yv = 0;
+        }
+        else if (A.x == 480 && A.y == 240) {
+            A.xv = 0;
+            A.yv = -1;
+        }
+        else if (A.y == 120 && A.x == 480) {
+            A.yv = 0;
+            A.xv = -1;
+        }  
+        else if (A.y == 120 && A.x == 320) {
+            A.xv = 0;
+            A.yv = 1;
+        }
+        A.x += A.xv; 
+        A.y += A.yv; 
+    });
+    B.move(() => {
+        if (B.x == 320 && B.y == 240) {
+            B.xv = -1;
+            B.yv = 0;
+        }
+        else if (B.x == 160 && B.y == 240) {
+            B.xv = 0;
+            B.yv = 1;
+        }
+        else if (B.y == 360 && B.x == 160) {
+            B.yv = 0;
+            B.xv = 1;
+        }  
+        else if (B.y == 360 && B.x == 320) {
+            B.xv = 0;
+            B.yv = -1;
+        }
+        B.x += B.xv; 
+        B.y += B.yv; 
+    });
+    A.draw();
+    B.draw();
+}
+
